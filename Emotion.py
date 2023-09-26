aaccc3f8ca1f19e5a57cb8266948334feef79f83
Tanish <mahajantanish28@gmail.com>
2023-09-25 20:08:41 -0400

    Minor changes

--- Emotion.py
+++ Emotion.py
@@ -33,7 +33,7 @@ while True:
             prediction_label = labels[pred.argmax()]
             # print("Predicted Output:", prediction_label)
             # cv2.putText(im,prediction_label)
-            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,2, (0,0,255))
+            cv2.putText(im, '% s' %(prediction_label), (p-10, q-10),cv2.FONT_HERSHEY_PLAIN_SMALL,2, (0,0,255))
         cv2.imshow("Output",im)
         cv2.waitKey(27)
     except cv2.error:
